3a09501094e26222a59f5f0b0ed1dc2e25227429
Uliana Kucherenko <ulya@Ulianas-MacBook-Air.local>
2023-11-01 23:33:43 +0200

    feat: add colors, theme

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,31 @@
 import { useDispatch } from 'react-redux';
+import { useState } from 'react';
+
 import type { AppDispatch } from './store';
 import { authThunks } from './store/user';
 import './App.css';
 import Home from './pages/home';
 
-function App() {
+const App = () => {
+  const [theme, setTheme] = useState<string>('light');
+  const toggleTheme = () => (theme === 'light' ? 'dark' : 'light');
+
   const dispatch = useDispatch<AppDispatch>();
   dispatch(authThunks.currentUser());
 
-  return <Home />;
-}
+  return (
+    <div className={`app ${theme === 'light' ? 'lightTheme' : ''}`}>
+      <button
+        type="button"
+        onClick={() => {
+          setTheme(toggleTheme);
+        }}
+      >
+        toggle theme
+      </button>
+      <Home />
+    </div>
+  );
+};
 
 export default App;
