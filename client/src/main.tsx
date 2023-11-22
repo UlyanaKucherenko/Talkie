5b0cefdea3c279bc12b23b5bdbbde96b24a59c2b
Uliana Kucherenko <ulya@Ulianas-MacBook-Air.local>
2023-11-22 15:58:35 +0200

    feat: comment my componentRoom, add chat store

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,7 +1,12 @@
-import React from 'react';
+import React /* useEffect, useState */ from 'react';
 import ReactDOM from 'react-dom/client';
-import { Provider } from 'react-redux';
-import { RouterProvider, createBrowserRouter } from 'react-router-dom';
+import { Provider /* useSelector */ } from 'react-redux';
+import {
+  RouterProvider,
+  createBrowserRouter,
+  // useParams,
+} from 'react-router-dom';
+// import io, { Socket } from 'socket.io-client';
 
 import App from './App';
 import store from './store';
@@ -10,6 +15,78 @@ import Home from './pages/home';
 import './libs/i18n';
 import Room from './pages/Room';
 import http from './api/http';
+// import { userSelector } from './store/user';
+// import { IMessage } from './store/chat';
+
+// const socket: Socket = io('http://localhost:3001');
+
+// const Room = () => {
+//   const params = useParams();
+//   const { userData } = useSelector(userSelector);
+//   const [messages, setMessages] = useState<IMessage[]>([]);
+//   const [inputMessage, setInputMessage] = useState<string>('');
+//   const roomId = params.id;
+
+//   useEffect(() => {
+//     // Subscribe to the new message event
+//     socket.on('message', (message: IMessage) => {
+//       setMessages((prevMessages) => [...prevMessages, message]);
+//     });
+
+//     // Subscribe to the event of receiving the list of messages
+//     socket.on('messageList', (messageList: IMessage[]) => {
+//       setMessages(messageList);
+//     });
+
+//     // Send a request to enter the room
+//     socket.emit('joinRoom', roomId);
+
+//     // Unsubscribe from events when unmounting the component
+//     return () => {
+//       socket.off('message');
+//       socket.off('messageList');
+//     };
+//   }, [roomId]);
+
+//   const sendMessage = () => {
+//     socket.emit('sendMessage', {
+//       id: Date.now(),
+//       msg: inputMessage,
+//       roomId,
+//       nick: userData?.user.name,
+//       date: Date.now(),
+//     });
+//     setInputMessage('');
+//   };
+
+//   return (
+//     <div>
+//       <h3>Room-ID: {params.id}</h3>
+//       <div style={{ display: 'flex', justifyContent: 'center' }}>
+//         <input
+//           type="text"
+//           placeholder="massage.."
+//           value={inputMessage}
+//           onChange={(e) => setInputMessage(e.target.value)}
+//         />
+//         <button type="button" onClick={sendMessage}>
+//           send
+//         </button>
+//       </div>
+//       <hr />
+
+//       <div>
+//         {messages.map((item) => (
+//           <p key={item.id}>
+//             <span>
+//               <b>{item.nick}</b>: {item.msg}
+//             </span>
+//           </p>
+//         ))}
+//       </div>
+//     </div>
+//   );
+// };
 
 const router = createBrowserRouter([
   {
@@ -20,6 +97,10 @@ const router = createBrowserRouter([
         path: '/',
         element: <Home />,
       },
+      // {
+      //   path: '/public-chat/:id',
+      //   element: <Room />,
+      // },
       {
         path: 'public-chat/:roomId',
         loader: async ({ params }) => {
@@ -28,6 +109,7 @@ const router = createBrowserRouter([
         },
         element: <Room />,
       },
+
       {
         path: '*',
         element: <div style={{ textAlign: 'center' }}>Error 404</div>,
