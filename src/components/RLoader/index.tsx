74088661b2cf6b2fe24704c41f6c395c8a26741d
Uliana Kucherenko <ulya@Ulianas-MacBook-Air.local>
2023-12-16 00:10:16 +0200

    feat: add new loader, add loader to chat

--- src/components/RLoader/index.tsx
+++ src/components/RLoader/index.tsx
@@ -1,7 +1,27 @@
 import styles from './index.module.css';
 
-function RLoader() {
-  return <div className={styles.loader}>Loading...</div>;
+type RLoaderProps = {
+  className?: string;
+  css?: {};
+  size?: 'sm' | 'md';
+};
+
+const sizeLoader = {
+  sm: '24px',
+  md: '48px',
+};
+
+function RLoader({ className, css, size = 'md' }: RLoaderProps) {
+  let classNameRButton = styles.loader;
+  if (className) {
+    classNameRButton = `${classNameRButton} ${className}`;
+  }
+  const stylesLouder = {
+    ...css,
+    width: sizeLoader[size],
+    height: sizeLoader[size],
+  };
+  return <div className={classNameRButton} style={stylesLouder} />;
 }
 
 export { RLoader };
